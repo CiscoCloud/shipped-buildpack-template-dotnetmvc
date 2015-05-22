64e14b18a57018250fa7b4ae47276779ef711ff8
Brad Gearon <bradgearon@gmail.com>
2015-05-22 16:07:24 -0500

    initial

---
+++ test/HelloMvc.Tests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Mvc;
+using MvcSample.Web;
+using MvcSample.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HelloMvc.Tests
+{
+    public class HomeControllerTests
+    {
+        private HomeController _homeController;
+        public HomeControllerTests()
+        {
+            _homeController = new HomeController();
+        }
+
+        [Fact]
+        public void IndexSetsUserModel()
+        {
+            var result = _homeController.Index() as ViewResult;
+            var model = result.ViewData.Model as User;
+            Assert.Null(model);
+        }
+    }
+}
